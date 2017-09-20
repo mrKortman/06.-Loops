a20641b615ab20750704a42f91b7b8afe383e842
Boy Kortman <b.kortman@leeuwenborgh.nl>
2017-09-20 10:29:29 +0200

    Eerste commit over het hoofdstuk 06. Loops

--- 06. Loops/Program.cs
+++ 06. Loops/Program.cs
@@ -6,10 +6,52 @@ using System.Threading.Tasks;
 
 namespace _06.Loops
 {
+    /* Dit programma vraagt om een getal onder de 100 en 
+       laat daar van de tafel op het scherm zien via een loop */
+
     class Program
     {
         static void Main(string[] args)
         {
+            bool blnContinue = true;
+            
+            //DO WHILE LOOP
+            do
+            {
+                double dblSum = 0;
+                double dblMultiplicationTable = 0;
+
+                Console.Clear();
+                Console.WriteLine("Geef een getal op onder de 100");
+                
+                dblMultiplicationTable = Convert.ToDouble(Console.ReadLine());
+
+                if (dblMultiplicationTable < 100)
+                {
+                    Console.WriteLine("Hier onder ziet u de tafel van {0}", dblMultiplicationTable.ToString());
+
+                    // FOR LOOP
+                    for (int i = 1; i < 10; i++)
+                    {
+                        dblSum = i * dblMultiplicationTable;
+                        Console.WriteLine("{0} x {1} = {2}", i, dblMultiplicationTable, dblSum);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("U heeft een getal boven de 100 opgegeven, probeer het nog eens.");
+                }
+
+                Console.WriteLine("Wilt u nog een tafel weergeven ? (j/n)");
+                string strContinue = Console.ReadLine();
+
+                // IF STATEMENT
+                if (strContinue == "n")
+                {
+                    blnContinue = false;
+                }
+            }
+            while (blnContinue);
         }
     }
 }
